5aef16c88cc55fd7feb91336c5227e5ef5c88ca8
ShraddhaSunilP <shraddhapatil2630@gmail.com>
2024-07-29 14:44:36 +0530

    API's Programming uploaded

---
+++ section-28/5.4+API+Authentication/solution.js
@@ -0,0 +1,70 @@
+import express from "express";
+import axios from "axios";
+
+const app = express();
+const port = 3000;
+const API_URL = "https://secrets-api.appbrewery.com";
+
+// TODO: Replace the values below with your own before running this file.
+const yourUsername = "shraddha";
+const yourPassword = "shraddha@123";
+const yourAPIKey = "f920606e-adb3-494f-a0c2-6986b44564cd";
+const yourBearerToken = "64d309dd-7976-4a49-9409-fe0a83a35f9c";
+
+app.get("/", (req, res) => {
+  res.render("index.ejs", { content: "API Response." });
+});
+
+app.get("/noAuth", async (req, res) => {
+  try {
+    const result = await axios.get(API_URL + "/random");
+    res.render("index.ejs", { content: JSON.stringify(result.data) });
+  } catch (error) {
+    res.status(404).send(error.message);
+  }
+});
+
+app.get("/basicAuth", async (req, res) => {
+  try {
+    const result = await axios.get(API_URL + "/all?page=2", {
+      auth: {
+        username: yourUsername,
+        password: yourPassword,
+      },
+    });
+    res.render("index.ejs", { content: JSON.stringify(result.data) });
+  } catch (error) {
+    res.status(404).send(error.message);
+  }
+});
+
+app.get("/apiKey", async (req, res) => {
+  try {
+    const result = await axios.get(API_URL + "/filter", {
+      params: {
+        score: 5,
+        apiKey: yourAPIKey,
+      },
+    });
+    res.render("index.ejs", { content: JSON.stringify(result.data) });
+  } catch (error) {
+    res.status(404).send(error.message);
+  }
+});
+
+const config = {
+  headers: { Authorization: `Bearer ${yourBearerToken}` },
+};
+
+app.get("/bearerToken", async (req, res) => {
+  try {
+    const result = await axios.get(API_URL + "/secrets/2", config);
+    res.render("index.ejs", { content: JSON.stringify(result.data) });
+  } catch (error) {
+    res.status(404).send(error.message);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
---
+++ section-28/Secrets Project/index.js
@@ -0,0 +1,37 @@
+// 1. Import express and axios
+import express from "express";
+import axios from "axios";
+
+// 2. Create an express app and set the port number.
+const app = express();
+const port = 3000;
+
+// 3. Use the public folder for static files.
+app.use(express.static("public"));
+
+app.get("/", async (req, res) => {
+    try {
+      const result = await axios.get("https://secrets-api.appbrewery.com/random");
+      res.render("index.ejs", {
+        secret: result.data.secret,
+        user: result.data.username,
+      });
+    } catch (error) {
+      console.log(error.response.data);
+      res.status(500);
+    }
+  });
+
+// 6. Listen on your predefined port and start the server.
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+
+
+
+
+
+
+
+
+
